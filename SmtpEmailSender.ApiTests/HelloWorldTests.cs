94572404b68c3a7fc29159b9c0da17ec4dab95b1
joncoello <joncoello@gmail.com>
2017-01-19 15:15:02 +0000

    add api and test project

---
+++ SmtpEmailSender.ApiTests/HelloWorldTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace SmtpEmailSender.ApiTests
+{
+    public class HelloWorldTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
