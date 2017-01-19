4d87b9cfab23e0c34d576453429200aec73efc84
joncoello <joncoello@gmail.com>
2017-01-19 16:04:28 +0000

    extend failing test to start server

--- SmtpEmailSender.ApiTests/HelloWorldTests.cs
+++ SmtpEmailSender.ApiTests/HelloWorldTests.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using SmtpEmailSender.Api;
+using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,13 +15,25 @@ namespace SmtpEmailSender.ApiTests
         public async Task HelloWorld_Get()
         {
 
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:8099");
-
-            var response = await client.GetAsync("/api/hello");
-
-            response.EnsureSuccessStatusCode();
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
             
+            string address = "http://localhost:" + port.ToString();
+
+            using (var server = WebApp.Start<Startup>(address))
+            {
+                
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(address);
+
+                var response = await client.GetAsync("/api/hello");
+
+                response.EnsureSuccessStatusCode();
+
+            }
+
         }
     }
 }
