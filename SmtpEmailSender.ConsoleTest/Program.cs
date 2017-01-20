d9d580472b9118b04e62dce7bcdbe91ab183d957
joncoello <joncoello@gmail.com>
2017-01-20 13:19:11 +0000

    move email sending logic to domain model

--- SmtpEmailSender.ConsoleTest/Program.cs
+++ SmtpEmailSender.ConsoleTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmtpEmailSender.DomainModel.Providers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -16,39 +17,8 @@ namespace SmtpEmailSender.ConsoleTest
             Console.WriteLine("Enter Password");
             string password = Console.ReadLine();
 
-            var client = new SmtpClient("smtp.gmail.com", 587);
-            client.EnableSsl = true;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new NetworkCredential("joncoello@gmail.com", password);
-
-            try
-            {
-                // Create instance of message
-                MailMessage message = new MailMessage();
-
-                // Add receiver
-                message.To.Add("jon.coello@wolterskluwer.com");
-
-                // Set sender
-                // In this case the same as the username
-                message.From = new MailAddress("joncoello@gmail.com");
-
-                // Set subject
-                message.Subject = "Test";
-
-                // Set body of message
-                message.Body = "En test besked";
-
-                // Send the message
-                client.Send(message);
-
-                // Clean up
-                message = null;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Could not send e-mail. Exception caught: " + e);
-            }
+            var smtpClient = new SmtpEmailProvider();
+            smtpClient.Send(password);
 
         }
     }
---
+++ SmtpEmailSender.DomainModel/Providers/SmtpEmailProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmtpEmailSender.DomainModel.Providers
+{
+    public class SmtpEmailProvider
+    {
+
+        public void Send(string password) {
+
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("joncoello@gmail.com", password);
+
+            try
+            {
+                // Create instance of message
+                MailMessage message = new MailMessage();
+
+                // Add receiver
+                message.To.Add("jon.coello@wolterskluwer.com");
+
+                // Set sender
+                // In this case the same as the username
+                message.From = new MailAddress("joncoello@gmail.com");
+
+                // Set subject
+                message.Subject = "Test";
+
+                // Set body of message
+                message.Body = "This is a test message";
+
+                // Send the message
+                client.Send(message);
+
+                // Clean up
+                message = null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not send e-mail. Exception caught: " + e);
+            }
+
+        }
+
+    }
+}
